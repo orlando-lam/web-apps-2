546ce91d75165a6af3d7af687b9ead87c751f6d1
orlando-lam <orlando.lam@kellogg.northwestern.edu>
2022-05-16 16:45:36 +0000

    v1

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,8 +4,27 @@ class CompaniesController < ApplicationController
   # def index
   # end
 
+  def index
+    @companies = Company.all
+
+  end
+
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    # render /views/companies/show.html.erb
+  end 
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["city"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
   end
 
 end
